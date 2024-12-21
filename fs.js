476c8191ae538633df56f61d336f6a47a48f6a10
bruceathy <67832593+bruceathy@users.noreply.github.com>
2024-12-20 22:03:17 -0500

    fs

--- fs.js
+++ fs.js
@@ -2,13 +2,17 @@ const fs = require("fs");
 
 // write to a file //
 
-// Callback version
-fs.writeFile("test.txt", "Hello World", (err) => {
-  if (err) throw err;
-  console.log("The file has been saved!");
-});
+// // Callback version
+// fs.writeFile("test.txt", "Hello World", (err) => {
+//   if (err) throw err;
+//   console.log("The file has been saved!");
+// });
 
-// Promise version
-fs.writeFile("test2.txt", "Hello World 2")
-  .then(() => console.log("The file has been saved!"))
-  .catch((err) => console.error(err));
+// // Promise version
+// fs.writeFile("test2.txt", "Hello World 2")
+//   .then(() => console.log("The file has been saved! Again?"))
+//   .catch((err) => console.error(err));
+
+// Sync version
+fs.writeFileSync("test3.txt", "Hello World 3");
+console.log("The file has been saved! Again!");
